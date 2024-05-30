fd9a4d2a5aba3ca8e78fb6d6436a4a34ac2fd69b
Shauli Ravfogel <shauli321@gmail.com>
2024-05-30 22:04:16 +0300

    Delete classification/encode_all.sh

--- classification/encode_all.sh
+++
@@ -1,6 +0,0 @@
-python3 encode.py --model meta-llama/Llama-2-7b-chat-hf --layer -1 --pooling last --data tweets --batch_size 32
-python3 encode.py --model google-bert/bert-base-uncased --layer -1 --pooling cls --data bios --batch_size 512
-python3 encode.py --model meta-llama/Llama-2-7b-chat-hf --layer -1 --pooling last --data bios --batch_size 32
-python3 encode.py --model openai-community/gpt2 --layer -1 --pooling last --data bios --batch_size 512
-
-
